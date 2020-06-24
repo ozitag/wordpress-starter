d7114e52b55bace7e042b880e38d1b645421b5a4
Vital Ozierski <v.ozierski@ozitag.com>
2020-06-24 12:05:43 +0300

    upload deploy scripts and gitlab-ci

---
+++ scripts/deploy-develop.sh
@@ -0,0 +1,15 @@
+cd {{PROJECT_PATH}}
+
+sudo -u www-data git fetch --all
+sudo -u www-data git reset --hard origin/develop
+sudo -u www-data git clean -f -d
+
+mv .env.development .env
+mv src/.env.development src/.env
+
+cd src/public/wp-content/themes/app/html
+yarn install
+yarn build
+
+cd {{PROJECT_PATH}}
+sudo docker-compose -f docker-compose.dev.yml --project-name {{PROJECT_NAME}} up --build --force-recreate -d
---
+++ scripts/deploy-master.sh
@@ -0,0 +1,20 @@
+cd {{PROJECT_PATH}}
+
+sudo chown ozitag:ozitag -R .
+
+sudo docker image prune -a --force
+sudo docker container prune --force
+
+git fetch --all
+git reset --hard origin/master
+git clean -f -d
+
+mv .env.production .env
+mv src/.env.production src/.env
+
+cd src/public/wp-content/themes/app/html
+yarn install
+yarn build
+
+cd {{PROJECT_PATH}}
+sudo docker-compose -f docker-compose.yml --project-name {{PROJECT_NAME}} up --build --force-recreate -d
