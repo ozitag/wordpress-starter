5f26cf9fa1d3fcf142a41a67b55cdf8a3e2d5f24
Vital Ozierski <v.ozierski@ozitag.com>
2020-12-13 17:54:18 +0300

    improve deploy scripts (add chromium installation)

--- scripts/deploy-develop.sh
+++ scripts/deploy-develop.sh
@@ -1,18 +1,19 @@
-cd {{PROJECT_PATH}}
+cd /srv/{{PROJECT_PATH}}
 
-sudo chown www-data:www-data -R .
+sudo chown gitlab:gitlab -R .
 
-sudo -u www-data git fetch --all
-sudo -u www-data git reset --hard origin/develop
-sudo -u www-data git clean -f -d
+sudo -u gitlab git fetch --all
+sudo -u gitlab git reset --hard origin/develop
+sudo -u gitlab git clean -f -d
 
-mv .env.development .env
-mv src/.env.development src/.env
+cp .env.development .env
+cp src/.env.development src/.env
 
 cd src/public/wp-content/themes/app/html
-sudo apt-get -y install libjpeg-dev libpng-dev libtiff-dev libgif-dev
-sudo -u www-data yarn install
-sudo -u www-data yarn build
+sudo apt -y install libjpeg-dev libpng-dev libtiff-dev libgif-dev ca-certificates fonts-liberation libappindicator3-1 libasound2 libatk-bridge2.0-0 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libnss3 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 lsb-release wget xdg-utils
+
+sudo -u gitlab yarn install
+sudo -u gitlab yarn build
 
 cd {{PROJECT_PATH}}
-sudo docker-compose -f docker-compose.dev.yml --project-name {{PROJECT_NAME}} up --build --force-recreate -d
+sudo docker-compose -f docker-compose.dev.yml --project-name awem up --build --force-recreate -d
--- scripts/deploy-master.sh
+++ scripts/deploy-master.sh
@@ -15,8 +15,10 @@ mv src/.env.production src/.env
 sudo apt install nodejs
 sudo apt install npm
 sudo npm install -g yarn
+
 cd src/public/wp-content/themes/app/html
-sudo apt-get -y install libjpeg-dev libpng-dev libtiff-dev libgif-dev
+
+sudo apt -y install libjpeg-dev libpng-dev libtiff-dev libgif-dev ca-certificates fonts-liberation libappindicator3-1 libasound2 libatk-bridge2.0-0 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libnss3 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 lsb-release wget xdg-utils
 yarn install
 yarn build
 
