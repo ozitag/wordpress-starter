03b7d26bf0a2156f9bfc597411f8613a2a23ac17
Vital Ozierski <v.ozierski@ozitag.com>
2020-12-25 15:47:30 +0300

    fix domain names

--- src/public/wp-content/themes/app/src/post-types.php
+++ src/public/wp-content/themes/app/src/post-types.php
@@ -5,19 +5,19 @@ function init_post_types()
 {
     /*
     $labels = array(
-        'name' => _x('Products', 'post type general name', 'your-plugin-textdomain'),
-        'singular_name' => _x('Product', 'post type singular name', 'your-plugin-textdomain'),
-        'menu_name' => _x('Products', 'admin menu', 'your-plugin-textdomain'),
-        'name_admin_bar' => _x('Product', 'add new on admin bar', 'your-plugin-textdomain'),
-        'add_new' => _x('Add new', 'book', 'your-plugin-textdomain'),
-        'add_new_item' => __('Add new product', 'your-plugin-textdomain'),
-        'new_item' => __('New product', 'your-plugin-textdomain'),
-        'edit_item' => __('Edit product', 'your-plugin-textdomain'),
-        'view_item' => __('View product', 'your-plugin-textdomain'),
-        'all_items' => __('All products', 'your-plugin-textdomain'),
-        'search_items' => __('Search products', 'your-plugin-textdomain'),
-        'not_found' => __('Products not found', 'your-plugin-textdomain'),
-        'not_found_in_trash' => __('Products not found in trash', 'your-plugin-textdomain')
+        'name' => _x('Products', 'post type general name', 'website-admin'),
+        'singular_name' => _x('Product', 'post type singular name', 'website-admin'),
+        'menu_name' => _x('Products', 'admin menu', 'website-admin'),
+        'name_admin_bar' => _x('Product', 'add new on admin bar', 'website-admin'),
+        'add_new' => _x('Add new', 'book', 'website-admin'),
+        'add_new_item' => __('Add new product', 'website-admin'),
+        'new_item' => __('New product', 'website-admin'),
+        'edit_item' => __('Edit product', 'website-admin'),
+        'view_item' => __('View product', 'website-admin'),
+        'all_items' => __('All products', 'website-admin'),
+        'search_items' => __('Search products', 'website-admin'),
+        'not_found' => __('Products not found', 'website-admin'),
+        'not_found_in_trash' => __('Products not found in trash', 'website-admin')
     );
 
     $args = array(
@@ -36,22 +36,22 @@ function init_post_types()
     register_post_type('product', $args);
 
     $labels = array(
-        'name' => _x('Category', 'taxonomy general name', 'textdomain'),
-        'singular_name' => _x('Category', 'taxonomy singular name', 'textdomain'),
-        'search_items' => __('Search categories', 'textdomain'),
-        'popular_items' => __('Popular categories', 'textdomain'),
-        'all_items' => __('All categories', 'textdomain'),
+        'name' => _x('Category', 'taxonomy general name', 'website-admin'),
+        'singular_name' => _x('Category', 'taxonomy singular name', 'website-admin'),
+        'search_items' => __('Search categories', 'website-admin'),
+        'popular_items' => __('Popular categories', 'website-admin'),
+        'all_items' => __('All categories', 'website-admin'),
         'parent_item' => null,
         'parent_item_colon' => null,
-        'edit_item' => __('Edit categories', 'textdomain'),
-        'update_item' => __('Update categories', 'textdomain'),
-        'add_new_item' => __('Add new categories', 'textdomain'),
-        'new_item_name' => __('Name of category', 'textdomain'),
-        'separate_items_with_commas' => __('Separate categories with commas', 'textdomain'),
-        'add_or_remove_items' => __('Add or remove category', 'textdomain'),
-        'choose_from_most_used' => __('Choose from most used', 'textdomain'),
-        'not_found' => __('Categories not found', 'textdomain'),
-        'menu_name' => __('Categories', 'textdomain'),
+        'edit_item' => __('Edit categories', 'website-admin'),
+        'update_item' => __('Update categories', 'website-admin'),
+        'add_new_item' => __('Add new categories', 'website-admin'),
+        'new_item_name' => __('Name of category', 'website-admin'),
+        'separate_items_with_commas' => __('Separate categories with commas', 'website-admin'),
+        'add_or_remove_items' => __('Add or remove category', 'website-admin'),
+        'choose_from_most_used' => __('Choose from most used', 'website-admin'),
+        'not_found' => __('Categories not found', 'website-admin'),
+        'menu_name' => __('Categories', 'website-admin'),
     );
 
     $args = array(
