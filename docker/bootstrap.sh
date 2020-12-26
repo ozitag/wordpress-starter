9213b6983316d32dca63b26316f2cfdde4d6babf
Vital Ozierski <v.ozierski@ozitag.com>
2020-12-26 16:44:02 +0300

    add permissions

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -4,5 +4,7 @@ cd /var/www/app/ || exit
 composer i --ignore-platform-reqs
 
 cd /var/www/app/public || exit
+chown application:application -R .
 find . -type f -exec chmod 644 {} +
 find . -type d -exec chmod 755 {} +
+chmod 600 wp-config.php
