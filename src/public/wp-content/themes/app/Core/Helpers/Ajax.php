dfbde519ccb293edb17443d8739fd5cce48ea9a7
Vital Ozierski <v.ozierski@ozitag.com>
2021-05-13 12:43:15 +0300

    add GET params to Ajax:getActionUrl

--- src/public/wp-content/themes/app/Core/Helpers/Ajax.php
+++ src/public/wp-content/themes/app/Core/Helpers/Ajax.php
@@ -4,8 +4,8 @@ namespace Core\Helpers;
 
 class Ajax
 {
-    public static function getActionUrl(string $action): string
+    public static function getActionUrl(string $action, array $queryParams = []): string
     {
-        return get_admin_url() . 'admin-ajax.php?action=' . $action;
+        return get_admin_url() . 'admin-ajax.php?action=' . $action . (empty($queryParams) ? '' : '&' . http_build_query($queryParams));
     }
 }
